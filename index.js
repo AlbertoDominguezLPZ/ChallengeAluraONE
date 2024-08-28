7a6aee43dfaf0c844bc3243cfb9a2682a5ff8d1d
Alberto Domínguez <85902205+AlbertoDominguezLPZ@users.noreply.github.com>
2024-08-27 18:50:02 -0600

    index.js

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ function encriptar() {
   let tituloMensaje = document.getElementById("titulo-mensaje");
   let parrafo = document.getElementById("parrafo");
   let muñeco = document.getElementById("muñeco");
-
   let textoCifrado = texto
     .replace(/e/gi, "enter")
     .replace(/i/gi, "imes")
     .replace(/a/gi, "ai")
     .replace(/o/gi, "ober")
     .replace(/u/gi, "ufat");
-
   if (texto.length != 0) {
     document.getElementById("texto").value = textoCifrado;
     tituloMensaje.textContent = "Texto encriptado con éxito";
@@ -20,6 +18,7 @@ function encriptar() {
     muñeco.src = "./img/muñeco.png";
     tituloMensaje.textContent = "Ningún mensaje fue encontrado";
     parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    alert("Debes ingresar algún texto");
     swal("Ooops!", "Debes ingresar un texto", "warning");
   }
 }
@@ -29,7 +28,6 @@ function desencriptar() {
   let tituloMensaje = document.getElementById("titulo-mensaje");
   let parrafo = document.getElementById("parrafo");
   let muñeco = document.getElementById("muñeco");
-
   let textoCifrado = texto
     .replace(/enter/gi, "e")
     .replace(/imes/gi, "i")
@@ -46,6 +44,7 @@ function desencriptar() {
       muñeco.src = "./img/muñeco.png";
       tituloMensaje.textContent = "Ningún mensaje fue encontrado";
       parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      alert("Debes ingresar algún texto");
       swal("Ooops!", "Debes ingresar un texto", "warning");
     }
 }
